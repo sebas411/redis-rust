b82de34e371d123e1aa6aa91fe6d62f6f09b274c
smaldonado411 <smaldonado@distelsa.com.gt>
2025-12-03 17:22:49 -0600

    Handle multiple connections

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
-use std::{env, io::{Read, Write}, net::TcpListener};
+use std::{env, io::{Read, Write}, net::{TcpListener, TcpStream}, thread};
 use anyhow::Result;
+
+fn handle_client(mut stream: TcpStream) -> Result<()> {
+    println!("Incoming connection from: {}", stream.peer_addr()?);
+    let mut buffer = [0u8; 1024];
+
+    loop {
+        let bytes_read = stream.read(&mut buffer)?;
+        if bytes_read == 0 {
+            println!("Client disconnected: {}", stream.peer_addr()?);
+            return Ok(())
+        }
+        let _line = String::from_utf8(buffer[..bytes_read].to_vec())?;
+        stream.write("+PONG\r\n".as_bytes())?;
+    }
+}
 
 fn main() -> Result<()> {
     let mut port = "6379".to_string();
@@ -10,17 +25,12 @@ fn main() -> Result<()> {
     
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                println!("accepted new connection");
-                let mut buffer = [0u8; 1024];
-                loop {
-                    let bytes_read = stream.read(&mut buffer)?;
-                    if bytes_read == 0 {
-                        break;
+            Ok(stream) => {
+                thread::spawn(move || {
+                    if let Err(e) = handle_client(stream) {
+                        eprintln!("Error handling client: {}", e)
                     }
-                    let _line = String::from_utf8(buffer[..bytes_read].to_vec())?;
-                    stream.write("+PONG\r\n".as_bytes())?;
-                }
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
